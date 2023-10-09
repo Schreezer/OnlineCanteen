fbf3561f97fd77e5679854a854bfbed24c0b173a
Schreezer <96696271+Schreezer@users.noreply.github.com>
2023-10-09 22:10:45 +0530

    m

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(LoginScreen());
+}
+
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
-  late ;
+  late String email;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home:Scaffold(
+    return MaterialApp(
+        home: Scaffold(
       appBar: AppBar(
         title: const Text('APP NAME'),
       ),
-      body:Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Login', style: TextStyle(color: Colors.black,fontSize: 50,),),
-            TextField(autofillHints:'Username',),
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 50,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                    hintText: 'Email',
+                    icon: Icon(Icons.email),
+                    iconColor: Colors.blueGrey),
+                onChanged: (value) {
+                  email = value;
+                },
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     ));
